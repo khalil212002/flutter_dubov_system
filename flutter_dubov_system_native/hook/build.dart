73419751c844a3b141636675fa63dffec8ef46ff
Khillo <khalilwarwar2@gmail.com>
2026-03-02 21:46:58 +0200

    update ffigen and build config and generate bindings file

--- flutter_dubov_system_native/hook/build.dart
+++ flutter_dubov_system_native/hook/build.dart
@@ -7,8 +7,21 @@ void main(List<String> args) async {
     final packageName = input.packageName;
     final cbuilder = CBuilder.library(
       name: packageName,
-      assetName: '${packageName}_bindings_generated.dart',
-      sources: ['src/$packageName.c'],
+      assetName: packageName,
+      language: Language.cpp,
+      sources: [
+        'src/flutter_dubov_system_native.cpp',
+        'src/CPPDubovSystem/DubovSystem/Player.cpp',
+        'src/CPPDubovSystem/DubovSystem/Tournament.cpp',
+        'src/CPPDubovSystem/DubovSystem/explain.cpp',
+        'src/CPPDubovSystem/DubovSystem/baku.cpp',
+        'src/CPPDubovSystem/DubovSystem/LinkedList.cpp',
+        'src/CPPDubovSystem/DubovSystem/graph util/Graph.cpp',
+        'src/CPPDubovSystem/DubovSystem/graph util/Matching.cpp',
+        'src/CPPDubovSystem/DubovSystem/graph util/BinaryHeap.cpp',
+        'src/CPPDubovSystem/DubovSystem/trf util/trf.cpp',
+        'src/CPPDubovSystem/DubovSystem/trf util/rtg.cpp',
+      ],
     );
     await cbuilder.run(
       input: input,
